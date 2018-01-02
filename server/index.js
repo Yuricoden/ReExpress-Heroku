e38ce17f0d7bb4cb015d6fbbbbb3181ed7b1a4a2
Erick Mwamodo <erick@merglabs.com>
2018-01-02 05:42:43 +0300

    Sepparation of api route to routers/api.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import api from "./routes/api";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,10 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
 
 // Answer API requests.
-app.get("/api", (req, res) => {
-  res.set("Content-Type", "application/json");
-  res.send('{"message":"Hello from the custom server!"}');
-});
+app.use("/api", api);
 
 // All remaining requests return the React app, so it can handle routing.
 app.get("*", (request, response) => {
